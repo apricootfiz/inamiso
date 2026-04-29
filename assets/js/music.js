fe24592697f512c4e0bf3b517f01a1e77bb0bac3
miso173 <k.apricoot@gmail.com>
2026-04-29 12:40:08 +0900

    2
    2

--- assets/js/music.js
+++ assets/js/music.js
@@ -92,10 +92,9 @@ function renderList() {
         row.className = "song-row";
 
         row.innerHTML = `
-          <div class="song-row-inner">
+          <label class="song-label">
 
-            <input type="checkbox" class="song-checkbox"
-                   value="${item.id}" data-stream="${streamId}">
+            <input type="checkbox" value="${item.id}" data-stream="${streamId}">
 
             <div class="song-info">
               <div class="song-main">
@@ -108,7 +107,7 @@ function renderList() {
               </div>
             </div>
 
-          </div>
+          </label>
         `;
 
         container.appendChild(row);
@@ -117,6 +116,11 @@ function renderList() {
     });
 
   });
+  
+  row.addEventListener("dblclick", () => {
+  playNow(item.id);
+  });
+  
 }
 
 
@@ -212,14 +216,6 @@ function startPlayback() {
 // ===============================================
 // クリック再生
 // ===============================================
-row.addEventListener("click", (e) => {
-
-  // ▼ チェックボックス押したときは無視
-  if (e.target.classList.contains("song-checkbox")) return;
-
-  playNow(item.id);
-});
-
 function playNow(itemId) {
   const item = playlist.find(p => p.id == itemId);
   if (!item) return;
@@ -279,23 +275,7 @@ function prevVideo() {
 function nextVideo() {
   if (!isPlaying) return;
 
-  // ▼ 割り込み再生中だった場合
-  if (isInterrupting) {
-    isInterrupting = false;
-
-    if (interruptBackup) {
-      selectedList = interruptBackup.list;
-      currentIndex = interruptBackup.index;
-
-      interruptBackup = null;
-
-      // ▼ 元のリストに戻る
-      loadVideo(currentIndex);
-      return;
-    }
-  }
-
-  // ▼ 通常処理
+  // ▼ 毎回再取得
   selectedList = getSelectedList();
 
   if (selectedList.length === 0) return;
