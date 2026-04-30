d27b6aba69a920bc74565e7b1731f90e290ae583
miso173 <k.apricoot@gmail.com>
2026-04-30 22:46:55 +0900

    a

--- assets/js/music.js
+++ assets/js/music.js
@@ -411,7 +411,7 @@ function loadVideo(index) {
 
   const nowPlaying = document.getElementById("nowPlaying");
   if (nowPlaying) {
-    nowPlaying.innerText = `再生中: ${item.song} - ${item.artist}`;
+    nowPlaying.innerText = `再生中: ${item.song} - ${item.artist} (${currentIndex + 1} / ${selectedList.length})`;
   }
 
   player.loadVideoById({
