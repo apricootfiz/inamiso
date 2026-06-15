a223c101f40c70d438b6e60bc0ed49d90711a910
miso173 <mi2.kv0@gmail.com>
2026-06-15 18:02:33 +0900

    a

--- assets/js/music.js
+++ assets/js/music.js
@@ -191,28 +191,57 @@ function openSongModal(stream) {
   // 曲を並べる
   stream.songs.forEach(song => {
 
-    const btn = document.createElement("button");
+    const btn = document.createElement("div");
 
     btn.className = "modal-song-btn";
 
-    btn.innerHTML = `
-      <div class="modal-song-name">
-        ${song.song}
-      </div>
+	btn.innerHTML = `
+	  <div class="modal-song-info">
+	    <div class="modal-song-name">
+	      ${song.song}
+	    </div>
 
-      <div class="modal-artist-name">
-        ${song.artist}
-      </div>
-    `;
+	    <div class="modal-artist-name">
+	      ${song.artist}
+	    </div>
+	  </div>
+
+	  <div class="modal-song-actions">
+	    <button class="modal-add-btn" type="button">
+		  <svg viewBox="0 -960 960 960" class="modal-icon">
+		    <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
+		  </svg>
+	    </button>
+
+	    <button class="modal-fav-btn" type="button">
+		  <svg viewBox="0 -960 960 960" class="modal-icon">
+		    <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
+		  </svg>
+	    </button>
+	  </div>
+	`;
+
+	const addBtn = btn.querySelector(".modal-add-btn");
+	const favBtn = btn.querySelector(".modal-fav-btn");
+
 
     // 曲クリックで再生
     btn.addEventListener("click", () => {
-
       closeSongModal();
-
       playNow(song.id);
-
     });
+
+	// ＋ボタンで再生リスト追加
+	addBtn.addEventListener("click", e => {
+	  e.stopPropagation();
+	  console.log("再生リストに追加", song);
+	});
+
+	// ハートボタンで再生リスト追加
+	favBtn.addEventListener("click", e => {
+	  e.stopPropagation();
+	  console.log("お気に入り登録", song);
+	});
 
     songList.appendChild(btn);
 
